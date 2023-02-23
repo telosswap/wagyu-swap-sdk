9242111e0759d47383efce592278d253fd773ce5
Altbee <liaojikundever@gmail.com>
2023-02-23 09:54:13 -0600

    update

--- dist/sdk.esm.js
+++ dist/sdk.esm.js
@@ -417,7 +417,7 @@ function Currency(decimals, symbol, name) {
  * The only instance of the base class `Currency`.
  */
 
-Currency.ETHER = /*#__PURE__*/new Currency(18, 'VLX', 'Velas VLX');
+Currency.ETHER = /*#__PURE__*/new Currency(18, 'TLOS', 'Telos');
 var ETHER = Currency.ETHER;
 
 var _WETH;
